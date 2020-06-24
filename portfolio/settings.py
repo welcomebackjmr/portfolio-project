14642dad3fa02f58344bf34c008eb0b5d8c3f6db
J.Man.R <jmrojas724@gmail.com>
2020-06-24 04:39:44 -0400

    mistake

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Angelique88?',
+        'PASSWORD':'django1234',
         'HOST':'localhost',
         'PORT':'5432',
 
